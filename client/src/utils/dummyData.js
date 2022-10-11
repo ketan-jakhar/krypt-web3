d8ae2576ddfb25cdb9bbd267ff6756367811d752
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-10-12 02:37:28 +0530

    finishing touches

--- client/src/utils/dummyData.js
+++ client/src/utils/dummyData.js
@@ -49,7 +49,7 @@ export default [
 		url: "https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif",
 		message: "",
 		timestamp: "12/21/2021, 4:33:21 PM",
-		addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+		addressFrom: `0xCF8e569A97C423952DdFf902375C7C76549A6A90`,
 		amount: "0.01",
 		addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
 	},
