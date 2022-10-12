60f81b7707d53f5f5520f40191774f0cd44ad0f9
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-10-12 16:54:05 +0530

    [fix]: minor fixes

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 
-const pk = "9d227ad1caa73054e11f3585fd3d088f95343f5b05c0fc9d9d3a547c36165d59";
 module.exports = {
 	solidity: "0.8.0",
 	networks: {
 		goerli: {
 			url: "https://eth-goerli.g.alchemy.com/v2/9h0jHXKC1fizIeFP1svGiQ3lpxo5xFD4",
-			accounts: [`0x${pk}`],
+			accounts: [
+				"0x9d227ad1caa73054e11f3585fd3d088f95343f5b05c0fc9d9d3a547c36165d59",
+			],
 		},
 	},
 };
