7d84ef5845184777ef3c0803e66e353738e7cfc7
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-10-12 17:04:47 +0530

    [update]: files update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,10 +35,7 @@ const Navbar = () => {
 				{navItems.map((item, index) => (
 					<NavBarItem key={item.title + index} title={item} />
 				))}
-				<li
-					className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'
-					// onClick={connectWallet}
-				>
+				<li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
 					{!currentAccount ? (
 						<button
 							type='button'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,12 @@
+require("dotenv").config;
 require("@nomiclabs/hardhat-waffle");
-
+require("@nomiclabs/hardhat-etherscan");
 module.exports = {
 	solidity: "0.8.0",
 	networks: {
 		goerli: {
 			url: "https://eth-goerli.g.alchemy.com/v2/9h0jHXKC1fizIeFP1svGiQ3lpxo5xFD4",
-			accounts: [
-				"0x9d227ad1caa73054e11f3585fd3d088f95343f5b05c0fc9d9d3a547c36165d59",
-			],
+			accounts: [`0x${import.meta.env.VITE_PK}`],
 		},
 	},
 };
